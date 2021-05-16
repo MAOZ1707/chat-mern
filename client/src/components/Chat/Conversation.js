9af31c6bdde2ea078ebf2fd7e79e24a35db44c62
Maoz.C <36860867+MAOZ1707@users.noreply.github.com>
2021-05-16 13:47:50 +0300

    add scroll to bottom

--- client/src/components/Chat/Conversation.js
+++ client/src/components/Chat/Conversation.js
@@ -1,18 +1,21 @@
 import React from 'react'
 import Message from './Message'
+import ScrollToBottom from 'react-scroll-to-bottom'
 
 import './Chat.css'
 
 const Conversation = ({ messageList, send }) => {
 	return (
-		<div className='chat__body'>
-			{messageList &&
-				messageList.map((msg, i) => (
-					<React.Fragment key={i}>
-						<Message content={msg} send={send} />
-					</React.Fragment>
-				))}
-		</div>
+		<ScrollToBottom className='messages'>
+			<div className='chat__body'>
+				{messageList &&
+					messageList.map((msg, i) => (
+						<React.Fragment key={i}>
+							<Message content={msg} send={send} />
+						</React.Fragment>
+					))}
+			</div>
+		</ScrollToBottom>
 	)
 }
 
